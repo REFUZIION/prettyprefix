ccfdb95540d9dc3c01cac5be3f85df94f4d5f374
REFUZIION <djdveenstra11@gmail.com>
2023-02-05 21:23:56 +0100

    Update event listener and load prettyprefix.html template

--- event/listener.php
+++ event/listener.php
@@ -9,65 +9,50 @@
  */
 namespace refuziion\prettyprefix\event;
 
-use phpbb\language\language;
+use phpbb\template\template;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class listener implements EventSubscriberInterface
 {
-	/**
-	 * @var language
-	 */
-	protected $language;
-
-	/**
-	 * Constructor
-	 *
-	 * @param \phpbb\language\language $language
-	 */
-	public function __construct(\phpbb\language\language $language)
-	{
-		$this->language = $language;
-	}
+    /** @var template */
+    protected $template;
 
     /**
-	 * @return array
-	 */
-	public static function getSubscribedEvents():array
+     * listener constructor.
+     * @param template $template
+     */
+    public function __construct(template $template)
+    {
+        $this->template = $template;
+		$this->template->set_style('refuziion/prettyprefix', 'ext/refuziion/prettyprefix/styles/');
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
     {
         return [
-            'core.posting_modify_template_vars' => 'add_prefix_template_var',
+            'core.posting_modify_template_vars' => 'modify_template_vars',
         ];
     }
 
-	/**
-	 * @param mixed $event
-	 * @return void
-	 */
-	public function load_language_on_setup($event):void
-	{
-		$lang_set_ext = $event['lang_set_ext'];
-		$lang_set_ext[] = [
-			'ext_name' => 'refuziion/prettyprefix',
-			'lang_set' => 'common',
-		];
-		$event['lang_set_ext'] = $lang_set_ext;
-	}
-
-	/**
-	 * @param mixed $event
-	 * @return mixed
-	 */
-    public function add_prefix_template_var($event)
+    /**
+     * @param \phpbb\event\data $event
+     */
+    public function modify_template_vars(\phpbb\event\data $event)
     {
-        $prefixes = [
-            'red' => 'PRETTYPREFIX_RED',
-            'green' => 'PRETTYPREFIX_GREEN',
-            'blue' => 'PRETTYPREFIX_BLUE',
-        ];
+        $this->template->assign_vars([
+            'S_PRETTY_TOPIC_PREFIX' => true,
+            'PRETTY_PREFIX_RED' => 'Red',
+            'PRETTY_PREFIX_GREEN' => 'Green',
+            'PRETTY_PREFIX_BLUE' => 'Blue',
+        ]);
 
-        $event['page_data']['prefixes'] = $prefixes;
-        $event['template_vars']['S_PRETTYPREFIX'] = true;
+		$this->template->set_filenames([
+			'prettyprefix' => 'prettyprefix.html',
+		]);
 
-		return $event;
-	}
+		$this->template->assign_display('prettyprefix');
+    }
 }
